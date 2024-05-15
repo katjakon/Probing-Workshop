47f3a3325c53e831b679ec34802f9c0c53797fc3
Mikhail Sonkin <michael.sonkin@gmail.com>
2024-05-16 00:29:01 +0200

    add probedict to DatasetProcessor

--- dataset_processor.py
+++ dataset_processor.py
@@ -1,5 +1,7 @@
 from datasets import Dataset, DatasetDict, load_dataset
-from transformers import BertPreTrainedModel, BertTokenizerFast
+from transformers import BertModel, BertTokenizerFast
+from tqdm import tqdm
+import torch
 
 class DatasetProcessor:
     """
@@ -19,22 +21,38 @@ class DatasetProcessor:
         model_name : str
             The name of the model we use to embed and tokenise the dataset.
             Must be possible to load with BertPreTrainedModel.
-        # TODO: write the rest
+        data_column : str
+            Column in the dataset that signifies the sentences/tokens
+        labels_column : str
+            Column in the dataset that signifies the labels
+        dataset_type : str
+            Type of data. Is either "tokens" or "sentences".
         """
         self.dataset = dataset
+
         self.model_name = model_name
         self.tokenizer = BertTokenizerFast.from_pretrained(model_name)
-        self.model = BertPreTrainedModel.from_pretrained(model_name)
+        self.model = BertModel.from_pretrained(model_name)
 
         # data columns
         self.data_column = data_column
         self.labels_column = labels_column
 
+        self.dataset_keys = list(dataset.keys())
+        self.label_tags = dataset[self.dataset_keys[0]].features[labels_column].feature.names
+
+        self.create_probedict()
+
         assert dataset_type in self.DATASET_TYPES
         self.dataset_type = dataset_type
+
+        # self.dataset_with_token_ids = dataset.map(lambda x: self._tokenize_and_align_labels(x), batched=True)
     
     @staticmethod
     def _align_labels_with_tokens(labels, word_ids, none_label=0):
+        """
+        Takes labels and word_ids to ali
+        """
         print(labels)
         new_labels = []
         current_word = None
@@ -53,7 +71,7 @@ class DatasetProcessor:
                 new_labels.append(label)
 
         return new_labels
-
+    
 
     def _tokenize_and_align_labels(self, examples):
         # TODO: make it so that the object has both tokens and labels after this
@@ -71,17 +89,64 @@ class DatasetProcessor:
 
         return tokenized_inputs
 
-    def embed(self):
-        """
-        Add the embeddings column to the dataset
-        """
-        pass
+    def create_probedict(self):
+        print("Creating dict for probing...")
+        self.probedict = {k: {"embeddings": [], "labels": [], "ids": []} for k in self.dataset.keys()}
+        for key in self.dataset.keys():
+            print(f"Embeddings for {key}")
+            data = self.dataset[key]
+            tokens = data[self.data_column]
+            labels = data[self.labels_column]
 
-    def labeled_list(self):
-        pass
+            for i, sentence in enumerate(tqdm(tokens)):
+                sentence_ids, sentence_mapping = self.convert_to_ids(sentence)
+                sentence_label_ids = labels[i]
+                sentence_embeddings = self.get_word_representations(sentence_ids, sentence_mapping)
+
+                # convert ids to string for readability
+                sentence_tokens = self.convert_to_tokens(sentence_ids)
+                sentence_labels = self.convert_to_labels(sentence_label_ids)
+
+                self.probedict[key]["ids"].extend(sentence_tokens)
+                self.probedict[key]["labels"].extend(sentence_labels)
+                self.probedict[key]["embeddings"].extend(sentence_embeddings)
+
+    def convert_to_tokens(self, ids):
+        return [self.tokenizer.decode(ix) for ix in ids]
+
+    def convert_to_labels(self, label_ids):
+        return [self.label_tags[ix] for ix in label_ids]
+
+    def convert_to_ids(self, string_list):
+        word_id = 1
+        mapping = []
+        subtokens = []
+        for string in string_list:
+            token_subtokens = self.tokenizer.tokenize(string)
+            subtokens.extend(token_subtokens)
+            mapping.append((word_id, word_id+len(token_subtokens)-1))
+            word_id += 1
+        ids = self.tokenizer.convert_tokens_to_ids(subtokens)
+        return ids, mapping
+
+    def get_word_representations(self, ids, mapping, layer_idx=-1):
+        ids = [101] + ids + [102]
+        ids = torch.tensor([ids])
+
+        output = self.model(ids, output_hidden_states=True)
+        output = torch.squeeze(output[layer_idx][0], dim=0)
+        # We don't need representations of [CLS] and [SEP]
+        output = output[:-1]
+
+        # taking the representation of the first subtoken
+        token_repr = torch.zeros(size=(len(mapping), output.shape[-1]))
+        for idx, (start, end) in enumerate(mapping):
+            token_repr[idx] += output[start]
+        return token_repr
 
 if __name__ == "__main__":
+    # pass
     data = load_dataset("universal_dependencies", "en_pronouns")
     bertmodel = "google-bert/bert-base-multilingual-cased"
     data_processor = DatasetProcessor(data, bertmodel, "tokens", "upos", dataset_type="tokens")
-    print(data_processor)
+    # print(data_processor)
